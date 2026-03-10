63ac848330da75dffd1114c48e8bbe4e17581106
Miqdad Somji <miqdad.somji@msph.uk>
2026-03-10 22:55:12 +0000

    Initial Commit

---
+++ Namaz Bar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Namaz Bar
+//
+//  Created by Miqdad Somji on 10/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Namaz Bar/Namaz_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Namaz_BarApp.swift
+//  Namaz Bar
+//
+//  Created by Miqdad Somji on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Namaz_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
